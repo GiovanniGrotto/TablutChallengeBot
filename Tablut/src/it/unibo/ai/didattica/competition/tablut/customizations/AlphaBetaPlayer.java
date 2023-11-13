263c3ee0af2f7074169ea9b0385bc3323b31a005
giovanni <giovannijgrotto@gmail.com>
2023-11-13 20:59:36 +0100

    Test con euristica a caso

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/AlphaBetaPlayer.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/AlphaBetaPlayer.java
@@ -5,6 +5,10 @@ import aima.core.search.adversarial.IterativeDeepeningAlphaBetaSearch;
 import it.unibo.ai.didattica.competition.tablut.domain.Action;
 import it.unibo.ai.didattica.competition.tablut.domain.State;
 
+import java.util.*;
+
+import static java.lang.Math.min;
+
 
 public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomState, Action, CustomState.Turn> {
 
@@ -20,12 +24,14 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
         /*if(this.counter%100000==0){ // ogni 100000 evaluation sovrascrive la hashmap local con quella aggiornata
             this.game.getPlayers();
         }
-        this.counter++;*/
+        this.counter++;
         long start = System.currentTimeMillis();
         super.eval(state, player);
         double eval= this.game.getUtility(state, player);
         this.evalTime += System.currentTimeMillis() - start;
-        return eval;
+        return eval;*/
+        super.eval(state, player);
+        return heuristicEvaluation(state);
     }
 
     @Override
@@ -52,5 +58,34 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
 
     public long getTimeInSeconds(long startTime){
         return (System.currentTimeMillis()-startTime)/1000;
+    }
+
+    public int heuristicEvaluation(CustomState state){
+        Integer whitePieces = 0;
+        Integer blackPieces = 0;
+        List<Integer> escapeTileX = new ArrayList<>(Arrays.asList(0, 1, 7, 8, 0, 8, 0, 8, 0, 1, 7, 8));
+        List<Integer> escapeTileY = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 1, 1, 7, 7, 8, 8, 8, 8));
+        Integer kingX = 0;
+        Integer kingY = 0;
+        Integer minEscapeDistance = 100;
+
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                State.Pawn piece = state.getBoard()[i][j];
+                if(piece == State.Pawn.WHITE) whitePieces++;
+                if(piece == State.Pawn.BLACK) blackPieces++;
+                if(piece == State.Pawn.KING){
+                    kingX = i;
+                    kingY = j;
+                }
+            }
+        }
+
+        for(int i = 0; i < escapeTileX.size(); i++) {
+            minEscapeDistance = Math.min(minEscapeDistance, Math.abs(kingX - escapeTileX.get(i)) + Math.abs(kingY - escapeTileY.get(i)));
+        }
+
+        return whitePieces-blackPieces+(7-minEscapeDistance);
     }
 }
