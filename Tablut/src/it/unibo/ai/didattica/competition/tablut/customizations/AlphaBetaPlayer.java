8fd7f9b0b3bf174c0b1c6f7a63654f9d31577e32
giovanni <giovannijgrotto@gmail.com>
2023-11-17 20:29:05 +0100

    Pulizieeeeee

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/AlphaBetaPlayer.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/AlphaBetaPlayer.java
@@ -16,14 +16,6 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
     int startBlackPieces = 0;
     Boolean foundWin = false;
     Boolean foundLoss = false;
-    Set<String> captureStates = new HashSet<>();
-    int kingTile = -1;
-    Set<Integer> whitePiecesSet = new HashSet<>();
-    Set<Integer> blackPiecesSet = new HashSet<>();
-    Set<Integer> rowIntestingTile = new HashSet<>(Arrays.asList(new Integer[]{3, 4, 5, 13, 27, 35, 36, 37, 43, 44, 45, 53, 67, 75, 76, 77, 40}));
-    Set<Integer> colIntestingTile = new HashSet<>(Arrays.asList(new Integer[]{3, 4, 5, 13, 27, 35, 36, 37, 43, 44, 45, 53, 67, 75, 76, 77, 40}));
-    Set<Integer> bothIntestingTile = new HashSet<>(Arrays.asList(new Integer[]{3, 4, 5, 13, 27, 35, 36, 37, 43, 44, 45, 53, 67, 75, 76, 77, 40}));
-
 
 
     public AlphaBetaPlayer(Game<CustomState, Action, CustomState.Turn> game , double utilMin, double utilMax, int time) {
@@ -31,11 +23,9 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
     }
 
     //Preso uno stato in input determina se sono avvenute catture rispetto allo stato root
-    public double evalCapture(CustomState state, State.Turn turn){
+    public double evalCapture(CustomState state, State.Turn player){
         int whitePieces = 0;
         int blackPieces = 0;
-        whitePiecesSet.clear();
-        blackPiecesSet.clear();
 
         State.Pawn[][] board = state.getBoard();
         for(int i = 0; i < board.length; i++) {
@@ -43,13 +33,8 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
                 State.Pawn piece = board[i][j];
                 if (Objects.equals(piece, State.Pawn.WHITE)) {
                     whitePieces++;
-                    this.whitePiecesSet.add(i * 9 + j);
                 }else if (Objects.equals(piece, State.Pawn.BLACK)) {
                     blackPieces++;
-                    this.blackPiecesSet.add(i * 9 + j);
-                }
-                else if(Objects.equals(piece, State.Pawn.KING)){
-                    this.kingTile = i * 9 + j;
                 }
             }
         }
@@ -57,107 +42,31 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
         int whitePieceDifference = whitePieces-startWhitePieces;
         int blackPieceDifference = blackPieces-startBlackPieces;
         if(state.getTurn() == State.Turn.WHITE){
-            if(whitePieceDifference > blackPieceDifference)
-                return (double) (-1 * blackPieceDifference) /this.currDepthLimit;
-            else if (whitePieceDifference < blackPieceDifference)
-                return (double) whitePieceDifference /this.currDepthLimit;
+            if(whitePieceDifference < blackPieceDifference)
+                return (double) (whitePieceDifference - blackPieceDifference) /this.currDepthLimit;
+            else if (whitePieceDifference > blackPieceDifference)
+                return (double) -1*(blackPieceDifference - whitePieceDifference) /this.currDepthLimit;
         }else {
-            if(whitePieceDifference > blackPieceDifference)
-                return (double) (-1 * whitePieceDifference)/this.currDepthLimit;
-            else if (whitePieceDifference < blackPieceDifference)
-                return( double) (blackPieceDifference)/this.currDepthLimit;
+            if(whitePieceDifference < blackPieceDifference)
+                return (double) -1*(whitePieceDifference - blackPieceDifference)/this.currDepthLimit;
+            else if (whitePieceDifference > blackPieceDifference)
+                return( double) (blackPieceDifference - whitePieceDifference)/this.currDepthLimit;
         }
         return 0;
     }
 
-    //Given a state determine it will set foundWinLoss to true
-    public boolean checkDoubleTreats(State.Turn player){
-        if(this.rowIntestingTile.contains(kingTile)){
-            int kingRow = kingTile / 9;
-            if(player == State.Turn.WHITE) {
-                for(int enemyTile : blackPiecesSet){
-                    int enemyRow = enemyTile / 9;
-                    if (kingRow == enemyRow) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            }else{
-                for(int enemyTile : whitePiecesSet){
-                    int enemyRow = enemyTile / 9;
-                    if (kingRow == enemyRow) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            }
-            return true;
-        }else if(this.colIntestingTile.contains(kingTile)) {
-            int kingCol = kingTile % 9;
-            if (player == State.Turn.WHITE) {
-                for (int enemyTile : blackPiecesSet) {
-                    int enemyCol = enemyTile % 9;
-                    if (kingCol == enemyCol) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            } else {
-                for (int enemyTile : whitePiecesSet) {
-                    int enemyCol = enemyTile % 9;
-                    if (kingCol == enemyCol) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            }
-            return true;
-        }/*else if (this.bothIntestingTile.contains(kingTile)) {
-            int kingRow = kingTile / 9;
-            int  kingCol = kingTile % 9;
-            if (player == State.Turn.WHITE) {
-                for (int enemyTile : blackPiecesSet) {
-                    int enemyRol = enemyTile / 9;
-                    int enemyCol = enemyTile % 9;
-                    if (kingCol == enemyCol) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            } else {
-                int obstacol = 0;
-                for (int enemyTile : whitePiecesSet) {
-                    int kingRow = kingTile / 9;
-                    int enemyCol = enemyTile % 9;
-                    if (kingCol == enemyCol) {
-                        return false; // King is aligned in the same row or col with an enemy piece
-                    }
-                }
-            }
-            return true;
-        }*/
-        return false;
-    }
-
     @Override
     public double eval(CustomState state, State.Turn player) {
-        if(this.counter%100000==0){ // ogni 100000 evaluation sovrascrive la hashmap local con quella aggiornata
+        /*if(this.counter%100000==0){ // ogni 100000 evaluation sovrascrive la hashmap local con quella aggiornata
             this.game.getPlayers();
-        }
+        }*/
         this.counter++;
         long start = System.currentTimeMillis();
         double captureEval = evalCapture(state, player);
-        /*if(this.currDepthLimit == 1 && (captureEval==1.1 || captureEval == -1.1)){
-            if(captureEval == 1.1)
-                this.captureStates.add(state.toString());
-        }*/
         super.eval(state, player);
         double eval = this.game.getUtility(state, player) + captureEval;
-        /*if(checkDoubleTreats(player)) {
-            if (player == State.Turn.WHITE)
-                eval = Double.POSITIVE_INFINITY - 10;
-            else
-                eval = Double.NEGATIVE_INFINITY + 10;
-        }*/
-        if(this.currDepthLimit <= 3 && (eval == Double.NEGATIVE_INFINITY || eval == Double.NEGATIVE_INFINITY+10)){
+        if(eval <= Double.NEGATIVE_INFINITY+10 || eval >= Double.POSITIVE_INFINITY-10){
             this.foundLoss = true;
-        }
-        if(eval == Double.POSITIVE_INFINITY || eval == Double.POSITIVE_INFINITY-10){
             this.foundWin = true;
         }
         this.evalTime += System.currentTimeMillis() - start;
@@ -167,11 +76,8 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
     @Override
     public Action makeDecision(CustomState state) {
         long startTime = System.currentTimeMillis();
-        this.foundWin = false;
-        this.foundLoss = false;
         this.startWhitePieces = 0;
         this.startBlackPieces = 0;
-        this.captureStates.clear();
 
         //Annota quanti pezzi per ogni colore ci sono nella scacchiera
         State.Pawn[][] board = state.getBoard();
@@ -188,19 +94,6 @@ public class AlphaBetaPlayer extends IterativeDeepeningAlphaBetaSearch<CustomSta
 
         //L'albero inizia la computazione
         Action a = super.makeDecision(state);
-
-        //Se ho catture e non ho trovato vittorie/sconfitte allora faccio la cattura
-        if(!this.captureStates.isEmpty() && !this.foundWin && !this.foundLoss){
-            List<Action> actions = this.game.getActions(state);
-            for(Action action : actions){
-                CustomState tmpState = this.game.getResult(state.clone(), action);
-                if(this.captureStates.contains(tmpState.toString())) {
-                    System.out.println("Mangio un pezzo");
-                    System.out.println("Explored a total of " + getMetrics().get(METRICS_NODES_EXPANDED) + " nodes, reaching a depth limit of " + getMetrics().get(METRICS_MAX_DEPTH) + " in " + getTimeInSeconds(startTime) + " seconds");
-                    return action;
-                }
-            }
-        }
 
         if(this.foundWin) System.out.println("Trovata vittoria");
         if(this.foundLoss) System.out.println("Trovata sconfitta");
--- Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/GameModel.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/customizations/GameModel.java
@@ -13,7 +13,7 @@ public class GameModel implements aima.core.search.adversarial.Game<CustomState,
     private final LimitedHashMap<String, Double> stateEvaluationMap; //= new LimitedHashMap<>(1000000);
     {
         try {
-            stateEvaluationMap = new LimitedHashMap<>(2000000, "stateEvaluation.json", "stateEvaluation");
+            stateEvaluationMap = new LimitedHashMap<>(3000000, "stateEvaluation.json", "stateEvaluation");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -88,17 +88,17 @@ public class GameModel implements aima.core.search.adversarial.Game<CustomState,
             State.Turn winnerPlayer = state.getTurn();
             if (turn == State.Turn.WHITE) {
                 if (winnerPlayer.equals(State.Turn.WHITEWIN))
-                    return Double.POSITIVE_INFINITY;
+                    return Double.POSITIVE_INFINITY-10;
                 else if (winnerPlayer.equals(State.Turn.BLACKWIN)){
-                    return Double.NEGATIVE_INFINITY;
+                    return Double.NEGATIVE_INFINITY+10;
                 } else {
                     return 0;
                 }
             }else if (turn == State.Turn.BLACK) {
                 if (winnerPlayer.equals(State.Turn.WHITEWIN))
-                    return Double.NEGATIVE_INFINITY;
+                    return Double.NEGATIVE_INFINITY+10;
                 else if (winnerPlayer.equals(State.Turn.BLACKWIN)){
-                    return Double.POSITIVE_INFINITY;
+                    return Double.POSITIVE_INFINITY-10;
                 } else {
                     return 0;
                 }
@@ -198,108 +198,4 @@ public class GameModel implements aima.core.search.adversarial.Game<CustomState,
         }
         return legalMoves;
     }
-
-    /*public List<Action> getActionsBruteForce(CustomState state) {
-        State.Pawn[][] board = state.getBoard();
-        List<Piece> pieces = new ArrayList<>();
-        String turnString = state.getTurn().toString();
-
-        // Scorriamo la board e salviamo in pieces solo i pezzi del colore del turno attuale
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                String piece = board[i][j].toString();
-                if((Objects.equals(turnString, "W") && (Objects.equals(piece, "K") || Objects.equals(piece, "W"))) || (Objects.equals(turnString, "B") && Objects.equals(piece, "B"))){
-                    pieces.add(new Piece(turnString, i, j));
-                }
-            }
-        }
-
-        // calcolo le mosse che può fare ogni pezzo e genero altri pezzi lì
-        List<Action> allMoves = new ArrayList<>();
-        State.Turn turn = state.getTurn();
-        for(Piece piece : pieces) {
-            // prendo la posizione da dove parte il pezzo che sto considerando
-            String fromTile = convertToChessPosition(piece.row+1, piece.col+1);
-            try {
-                // Move up
-                for (int i = piece.row - 1; i >= 0; i--) {
-                    // prendo la posizione dove finirà il pezzo che sto considerando
-                    String toTile = convertToChessPosition(i+1, piece.col+1);
-                    allMoves.add(new Action(fromTile, toTile, turn));
-                }
-
-                // Move down
-                for (int i = piece.row + 1; i < 9; i++) {
-                    String toTile = convertToChessPosition(i+1, piece.col+1);
-                    allMoves.add(new Action(fromTile, toTile, turn));
-                }
-
-                // Move left
-                for (int j = piece.col - 1; j >= 0; j--) {
-                    String toTile = convertToChessPosition(piece.row+1, j+1);
-                    allMoves.add(new Action(fromTile, toTile, turn));
-                }
-
-                // Move right
-                for (int j = piece.col + 1; j < 9; j++) {
-                    String toTile = convertToChessPosition(piece.row+1, j+1);
-                    allMoves.add(new Action(fromTile, toTile, turn));
-                }
-            }catch (Exception e){
-                //e.printStackTrace();
-            }
-        }
-
-        //passo ogni azione per checkmove, se non ritorna eccezioni è legale e la aggiungo alla lista di mosse legali
-        List<Action> legalMoves = new ArrayList<>();
-        for(Action action: allMoves){
-            try{
-                state.getRules().checkMove(state.clone(), action);
-                legalMoves.add(action);
-            }catch (Exception e){
-                //e.printStackTrace();
-            }
-        }
-        return legalMoves;
-    }
-
-    public List<Action> getActionsEmptyTiles(CustomState state) {
-        State.Pawn[][] board = state.getBoard();
-        List<Piece> pieces = new ArrayList<>();
-        List<Piece> emptyTiles = new ArrayList<>();
-        State.Turn turn = state.getTurn();
-        Set<Integer> forbiddenTile = new HashSet<>(Arrays.asList(new Integer[]{3, 4, 5, 13, 27, 35, 36, 37, 43, 44, 45, 53, 67, 75, 76, 77, 40}));
-
-        // Scorriamo la board e salviamo in pieces solo i pezzi del colore del turno attuale e le celle vuote che troviamo
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                State.Pawn piece = board[i][j];
-                if ((Objects.equals(turn, State.Turn.WHITE) && (Objects.equals(piece, State.Pawn.WHITE) || Objects.equals(piece, State.Pawn.KING)))
-                        || (Objects.equals(turn, State.Turn.BLACK) && Objects.equals(piece, State.Pawn.BLACK))) {
-                    pieces.add(new Piece(turn.toString(), i, j));
-                } else if (Objects.equals(piece, State.Pawn.EMPTY) && !forbiddenTile.contains(i * 9 + j)) { //controllo di non aggiungere trono o camp dei neri dove sicuro non possono andare i pezzi con mosse legali
-                    emptyTiles.add(new Piece(State.Pawn.EMPTY.toString(), i, j));
-                }
-            }
-        }
-
-        //Per ogni pezzo creiamo una mossa che parte da dov'è e arriva in ognuna delle caselle vuote e controlliamo sia una mossa legale
-        List<Action> legalMoves = new ArrayList<>();
-        for(Piece piece : pieces){
-            String fromTile = convertToChessPosition(piece.row+1, piece.col+1);
-            for(Piece emptyTile : emptyTiles){
-                // Se sono cambiate sia la colonna che la riga è una mossa orizzontale quindi illegale per definizione
-                if(emptyTile.row!=piece.row && emptyTile.col!=piece.col) continue;
-                String toTile = convertToChessPosition(emptyTile.row+1, emptyTile.col+1);
-                try{
-                    Action action = new Action(fromTile, toTile, turn);
-                    state.getRules().checkMove(state.clone(), action);
-                    legalMoves.add(action);
-                }catch (Exception e){
-                    //e.printStackTrace();
-                }
-            }
-        }
-        return legalMoves;
-    }*/
 }
